b532d3d2d956aaffdc58af63196657867aebd989
hosoyanaomi <cppbt124@yahoo.co.jp>
2024-11-20 14:03:47 +0900

    SpringBootプロジェクト作成

---
+++ MyInternProject/src/main/java/overtime/example/MyInternProjectApplication.java
@@ -0,0 +1,13 @@
+package overtime.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyInternProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyInternProjectApplication.class, args);
+	}
+
+}
